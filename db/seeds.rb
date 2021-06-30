b3156cc3609d46c110ad1b42cd3d4176730130a7
Jenny Kim <kimj1905@gmail.com>
2021-06-29 23:42:36 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create(title: "Kim K fails bar exam", description: "Kim K fails to follow her father's path in becoming a lawyer.")
